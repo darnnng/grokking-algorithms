9fc6bd40317ebc79698c3d3fd3e1295eaba85a48
Daria <dar.nagaj@gmail.com>
2022-11-21 21:15:24 +0300

    add 1 chapter

---
+++ js/selectionsort.js
@@ -0,0 +1,19 @@
+
+const findSmallest=(arr)=>{
+    let smallest=arr[0];
+    let smallestIndex=0;
+    for (let i=0; i<arr.length;i++) {
+        if (arr[i]<smallest) {
+            smallest=arr[i];
+            smallestIndex=i;
+        }
+    }
+    return smallestIndex
+}
+
+const selectionSort=(arr)=>{
+    let newArr=[];
+    for (let i=0; i<arr.length;i++) {
+        
+    }
+}
